472b45987c16a2a3e564fea56407ff3f94301083
Frode Hansen <frode.hansen@bekk.no>
2019-05-22 13:47:21 +0200

    RadioFix

--- src/modules/SelvstendigNæringsdrivendeBolk.ts
+++ src/modules/SelvstendigNæringsdrivendeBolk.ts
@@ -4,18 +4,16 @@ import SelvstendigNæringsdrivendeDialog from './SelvstendigN\u00E6ringsdrivende
 
 class SelvstendigNæringsdrivendeBolk {
     harJobbetJaRb: Selector;
-    harJobbetNeiRb: Selector;
     leggTilBt: Selector;
 
     constructor() {
         this.harJobbetJaRb = StegSelectors.radioPanelElement('harJobbetSomSelvstendigNæringsdrivendeSiste10Mnd', 'ja');
-        this.harJobbetNeiRb = StegSelectors.radioPanelElement('harJobbetSomSelvstendigNæringsdrivendeSiste10Mnd', 'ja');
         this.leggTilBt = Selector('button[data-name="leggTilNæring"]');
     }
 
     async fyllUtNorskregistrert(t: TestController) {
         const dialog = new SelvstendigNæringsdrivendeDialog();
-        await t.click(this.harJobbetJaRb).click(this.leggTilBt);
+        await t.click(this.harJobbetJaRb.parent('label')).click(this.leggTilBt);
         await dialog.fyllUt(t);
     }
 }
